8a782e49321cd3e5a0d307375ec34926e12276b1
g-leone <garrett.leone@gmail.com>
2020-09-10 18:43:38 -0700

    simple side bar

--- sidebar/side-bar-react/src/App.js
+++ sidebar/side-bar-react/src/App.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Sidebar from './components/Sidebar';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Reports from './pages/Reports'
+import Products from './pages/Products'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <>
+      <Router>
+        <Sidebar />
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/reports" component={Reports}/>
+          <Route path="/products" component={Products}/>
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
 export default App;
